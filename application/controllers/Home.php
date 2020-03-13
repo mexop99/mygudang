4c5a1f9d9b5047f6542eec85c1225c85cd0e15d4
mexop99 <mexop99@gmail.com>
2020-03-13 17:02:15 +0700

    sidebar-and-login
    custom side bar and standart form login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,9 @@
 <?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller{
     public function index()
     {
-        $data['title'] = "Home";
+        $data['title'] = "MyGudang | Home";
         $this->load->view('template/header', $data);
         $this->load->view('home/index');
         $this->load->view('template/footer');
